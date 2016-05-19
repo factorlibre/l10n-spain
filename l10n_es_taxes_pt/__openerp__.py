a5d95062c58bdc9be9b57a624bf69f52d8c79cfd
Hugo Santos <hugo.santos@factorlibre.com>
2016-05-19 17:21:15 +0200

    [ADD] l10n_es_taxes_pt

---
+++ l10n_es_taxes_pt/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# © 2016 FactorLibre - Hugo Santos <hugo.santos@factorlibre.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Spanish accounting configuration for Portugal Taxes',
+    'version': '8.0.1.0.0',
+    'depends': [
+        'l10n_es'
+    ],
+    'author': 'Odoo Community Association (OCA),FactorLibre',
+    'category': 'Localization/Account Charts',
+    'license': 'AGPL-3',
+    'website': 'http://www.factorlibre.com',
+    'data': [
+        'data/taxes_pt.xml',
+        'data/fiscal_position_pt.xml'
+    ],
+    'installable': True,
+    'application': False
+}
