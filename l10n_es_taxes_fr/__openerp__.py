92a330f6ef813207c1239f69862cda7d27a9eb23
Hugo Santos <hugo.santos@factorlibre.com>
2016-05-19 17:47:13 +0200

    [ADD] l10n_es_taxes_fr

---
+++ l10n_es_taxes_fr/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# © 2016 FactorLibre - Hugo Santos <hugo.santos@factorlibre.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Spanish accounting configuration for France Taxes',
+    'version': '8.0.1.0.0',
+    'depends': [
+        'l10n_es'
+    ],
+    'author': 'Odoo Community Association (OCA),FactorLibre',
+    'category': 'Localization/Account Charts',
+    'license': 'AGPL-3',
+    'website': 'http://www.factorlibre.com',
+    'data': [
+        'data/taxes_fr.xml',
+        'data/fiscal_position_fr.xml'
+    ],
+    'installable': True,
+    'application': False
+}
