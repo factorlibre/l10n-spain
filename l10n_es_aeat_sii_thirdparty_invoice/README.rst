d219e5f96756aed32b6772d874495eeed44a7436
Gerardo Gómez-Caminero Ortega <gerardo.gomez@factorlibre.com>
2021-01-20 18:09:36 +0100

    [MIG] l10n_es_aeat_sii_thirdparty_invoice: Migration from 8.0 to 11.0

---
+++ l10n_es_aeat_sii_thirdparty_invoice/README.rst
@@ -0,0 +1,57 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+===================================================================================
+Suministro Inmediato de Información en el IVA - Auto Facturas emitidas por terceros
+===================================================================================
+
+Ampliación módulo para la presentación inmediata del IVA para auto facturas generadas por terceros.
+
+
+Installation
+============
+
+Sin requisitos especiales.
+
+Configuration
+=============
+
+Sin requisitos especiales.
+
+Usage
+=====
+
+Al generar una factura emitida por terceros tenemos la opción de marcarla como Auto Factura en la sección SII de la factura y se puede indicar un número diferente.
+
+Known issues / Roadmap
+======================
+
+Ninguno
+
+Bug Tracker
+===========
+
+
+
+Credits
+=======
+
+Images
+------
+
+
+Contributors
+------------
+
+* Gerardo Gómez-Caminero <gerardo.gomez@factorlibre.com>
+
+Funders
+-------
+
+The development of this module has been financially supported by:
+
+* FactorLibre, S.L.
+
+Maintainer
+----------
---
+++ l10n_es_aeat_sii_thirdparty_invoice/tests/test_l10n_es_aeat_sii_self_invoice.py
@@ -0,0 +1,84 @@
+# © 2020 FactorLibre - Gerardo Gomez-Caminero <gerardo.gomez@factorlibre.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo.tests import common
+from odoo import exceptions, fields
+
+
+class TestAccountInvoice(common.TransactionCase):
+
+    def setUp(self):
+        super().setUp()
+        self.AccountInvoice = self.env['account.invoice']
+        self.ResCompany = self.env['res.company']
+        self.ResPartner = self.env['res.partner']
+        self.invoice = self.AccountInvoice.new({
+            'date_invoice': '2021-10-01',
+            'date': '2021-01-01',
+            'company_id': self.ResCompany.new({
+                'vat': 'ES123456789',
+            }),
+            'partner_id': self.ResPartner.new({
+                'commercial_partner_id': self.ResPartner.new({
+                    'name': 'NAMETEST_COMERCIALPARTNER',
+                }),
+            }),
+        })
+
+    def test_sii_force_number(self):
+        invoice = self.invoice
+        invoice.sii_force_number = 'TESTNUMBER'
+
+        self.assertEqual('TESTNUMBER', invoice.internal_number)
+
+    def test_get_sii_invoice_dict_with_thirdparty_number_is_ok(self):
+        invoice = self.invoice
+        invoice.sii_force_number = 'TESTNUMBER'
+        invoice.sii_thirdparty_invoice = True
+
+        inv_dict = invoice._get_sii_invoice_dict_out()
+        self.assertEqual(
+            'TESTNUMBER',
+            inv_dict.get(
+                "IDFactura",
+                {}).get("NumSerieFacturaEmisor", False)
+        )
+
+    def test_get_sii_invoice_dict_with_thirdparty_code_is_ok(self):
+        invoice = self.invoice
+        invoice.sii_force_number = 'TESTNUMBER'
+        invoice.sii_thirdparty_invoice = True
+
+        inv_dict = invoice._get_sii_invoice_dict_out()
+        self.assertEqual(
+            'S',
+            inv_dict.get(
+                "FacturaExpedida",
+                {}).get("EmitidaPorTercerosODestinatario", False)
+        )
+
+    def test_get_sii_invoice_dict_normal_number_ok(self):
+        invoice = self.invoice
+        invoice.number = 'TEST_NORMALNUMBER'
+        invoice.sii_force_number = 'TEST_THIRDPARTYNUMBER'
+        invoice.sii_thirdparty_invoice = False
+
+        inv_dict = invoice._get_sii_invoice_dict_out()
+        self.assertEqual(
+            'TEST_NORMALNUMBER',
+            inv_dict.get(
+                "IDFactura",
+                {}).get("NumSerieFacturaEmisor", False)
+        )
+
+    def test_get_sii_invoice_dict_normal_no_thirdparty_code(self):
+        invoice = self.invoice
+        invoice.sii_thirdparty_invoice = False
+
+        inv_dict = invoice._get_sii_invoice_dict_out()
+        self.assertEqual(
+            'EsNormal',
+            inv_dict.get(
+                "FacturaExpedida",
+                {}).get("EmitidaPorTercerosODestinatario", 'EsNormal')
+        )
