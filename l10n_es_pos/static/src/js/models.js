d238117a4b472fbed42a66e5f1bd32f9d98780b3
Daniel Duque <daniel.duque@factorlibre.com>
2022-11-18 09:48:38 +0100

    [MIG] l10n_es_pos: Migration to 16.0

--- l10n_es_pos/static/src/js/models.js
+++ l10n_es_pos/static/src/js/models.js
@@ -6,136 +6,139 @@
 odoo.define("l10n_es_pos.models", function (require) {
     "use strict";
 
-    var models = require("point_of_sale.models");
+    const {PosGlobalState, Order} = require("point_of_sale.models");
+    const Registries = require("point_of_sale.Registries");
 
-    var pos_super = models.PosModel.prototype;
-    models.PosModel = models.PosModel.extend({
-        initialize: function () {
-            pos_super.initialize.apply(this, arguments);
-            this.pushed_simple_invoices = [];
+    const L10NESGlobalState = (PosGlobalState) =>
+        class L10NESGlobalState extends PosGlobalState {
+            constructor() {
+                super(...arguments);
+                this.pushed_simple_invoices = [];
 
-            this.own_simplified_invoice_prefix = ""; // Unique UUID
-            return this;
-        },
-        get_simple_inv_next_number: function () {
-            return this.rpc({
-                method: "search_read",
-                domain: [["id", "=", this.config_id]],
-                fields: ["l10n_es_simplified_invoice_number"],
-                model: "pos.config",
-            });
-        },
-        get_padding_simple_inv: function (number) {
-            var diff =
-                this.config.l10n_es_simplified_invoice_padding -
-                number.toString().length;
-            var result = "";
-            if (diff <= 0) {
-                result = number;
-            } else {
-                for (var i = 0; i < diff; i++) {
-                    result += "0";
-                }
-                result += number;
+                this.own_simplified_invoice_prefix = ""; // Unique UUID
             }
-            return result;
-        },
-        push_simple_invoice: function (order) {
-            if (
-                this.pushed_simple_invoices.indexOf(order.data.l10n_es_unique_id) === -1
-            ) {
-                this.pushed_simple_invoices.push(order.data.l10n_es_unique_id);
-                ++this.config.l10n_es_simplified_invoice_number;
-            }
-        },
-        _flush_orders: function (orders) {
-            var self = this;
-            // Save pushed orders numbers
-            _.each(orders, function (order) {
-                if (!order.data.to_invoice) {
-                    self.push_simple_invoice(order);
-                }
-            });
-            return pos_super._flush_orders.apply(this, arguments);
-        },
-    });
-
-    var order_super = models.Order.prototype;
-    models.Order = models.Order.extend({
-        get_total_with_tax: function () {
-            var total = order_super.get_total_with_tax.apply(this, arguments);
-            var below_limit = total <= this.pos.config.l10n_es_simplified_invoice_limit;
-            this.is_simplified_invoice =
-                below_limit && this.pos.config.iface_l10n_es_simplified_invoice;
-            return total;
-        },
-        set_simple_inv_number: function () {
-            const self = this;
-            return this.pos
-                .get_simple_inv_next_number()
-                .then(function (configs) {
-                    const config = configs[0];
-                    self.pos.config.l10n_es_simplified_invoice_number =
-                        config.l10n_es_simplified_invoice_number;
-                    const simplified_invoice_number =
-                        self.pos.config.l10n_es_simplified_invoice_prefix +
-                        self.pos.get_padding_simple_inv(
-                            config.l10n_es_simplified_invoice_number
-                        );
-                    self.l10n_es_unique_id = simplified_invoice_number;
-                    self.is_simplified_invoice = true;
-                })
-                .catch(function () {
-                    self.l10n_es_unique_id = self.uid;
-                    self.is_simplified_invoice = true;
+            get_simple_inv_next_number() {
+                return this.env.services.rpc({
+                    method: "search_read",
+                    domain: [["id", "=", this.env.pos.config.id]],
+                    fields: ["l10n_es_simplified_invoice_number"],
+                    model: "pos.config",
                 });
-        },
-        get_base_by_tax: function () {
-            var base_by_tax = {};
-            this.get_orderlines().forEach(function (line) {
-                var tax_detail = line.get_tax_details();
-                var base_price = line.get_price_without_tax();
-                if (tax_detail) {
-                    Object.keys(tax_detail).forEach(function (tax) {
-                        if (Object.keys(base_by_tax).includes(tax)) {
-                            base_by_tax[tax] += base_price;
-                        } else {
-                            base_by_tax[tax] = base_price;
-                        }
-                    });
+            }
+            get_padding_simple_inv(number) {
+                const diff =
+                    this.config.l10n_es_simplified_invoice_padding -
+                    number.toString().length;
+                let result = "";
+                if (diff <= 0) {
+                    result = number;
+                } else {
+                    for (let i = 0; i < diff; i++) {
+                        result += "0";
+                    }
+                    result += number;
                 }
-            });
-            return base_by_tax;
-        },
-        init_from_JSON: function (json) {
-            order_super.init_from_JSON.apply(this, arguments);
-            this.to_invoice = json.to_invoice;
-            this.l10n_es_unique_id = json.l10n_es_unique_id;
-        },
-        export_as_JSON: function () {
-            var res = order_super.export_as_JSON.apply(this, arguments);
-            res.to_invoice = this.is_to_invoice();
-            if (!res.to_invoice) {
-                res.l10n_es_unique_id = this.l10n_es_unique_id;
+                return result;
             }
-            return res;
-        },
-        export_for_printing: function () {
-            var result = order_super.export_for_printing.apply(this, arguments);
-            var company = this.pos.company;
-            result.l10n_es_unique_id = this.l10n_es_unique_id;
-            result.to_invoice = this.to_invoice;
-            result.company.street = company.street;
-            result.company.zip = company.zip;
-            result.company.city = company.city;
-            result.company.state_id = company.state_id;
-            var base_by_tax = this.get_base_by_tax();
-            for (const tax of result.tax_details) {
-                tax.base = base_by_tax[tax.tax.id];
+            push_simple_invoice(order) {
+                if (
+                    this.pushed_simple_invoices.indexOf(
+                        order.data.l10n_es_unique_id
+                    ) === -1
+                ) {
+                    this.pushed_simple_invoices.push(order.data.l10n_es_unique_id);
+                    ++this.config.l10n_es_simplified_invoice_number;
+                }
             }
-            return result;
-        },
-    });
+            _flush_orders(orders) {
+                var self = this;
+                // Save pushed orders numbers
+                _.each(orders, function (order) {
+                    if (!order.data.to_invoice) {
+                        self.push_simple_invoice(order);
+                    }
+                });
+                return super._flush_orders(...arguments);
+            }
+        };
+    Registries.Model.extend(PosGlobalState, L10NESGlobalState);
 
-    models.load_fields("res.company", ["street", "city", "state_id", "zip"]);
+    const L10NESOrder = (Order) =>
+        class L10NESOrder extends Order {
+            get_total_with_tax() {
+                const total = super.get_total_with_tax(...arguments);
+                const below_limit =
+                    total <= this.pos.config.l10n_es_simplified_invoice_limit;
+                this.is_simplified_invoice =
+                    below_limit && this.pos.config.iface_l10n_es_simplified_invoice;
+                return total;
+            }
+            set_simple_inv_number() {
+                const self = this;
+                return this.pos
+                    .get_simple_inv_next_number()
+                    .then(function (configs) {
+                        const config = configs[0];
+                        self.pos.config.l10n_es_simplified_invoice_number =
+                            config.l10n_es_simplified_invoice_number;
+                        const simplified_invoice_number =
+                            self.pos.config.l10n_es_simplified_invoice_prefix +
+                            self.pos.get_padding_simple_inv(
+                                config.l10n_es_simplified_invoice_number
+                            );
+                        self.l10n_es_unique_id = simplified_invoice_number;
+                        self.is_simplified_invoice = true;
+                    })
+                    .catch(function () {
+                        self.l10n_es_unique_id = self.uid;
+                        self.is_simplified_invoice = true;
+                    });
+            }
+            get_base_by_tax() {
+                const base_by_tax = {};
+                this.get_orderlines().forEach(function (line) {
+                    const tax_detail = line.get_tax_details();
+                    const base_price = line.get_price_without_tax();
+                    if (tax_detail) {
+                        Object.keys(tax_detail).forEach(function (tax) {
+                            if (Object.keys(base_by_tax).includes(tax)) {
+                                base_by_tax[tax] += base_price;
+                            } else {
+                                base_by_tax[tax] = base_price;
+                            }
+                        });
+                    }
+                });
+                return base_by_tax;
+            }
+            init_from_JSON(json) {
+                super.init_from_JSON(...arguments);
+                this.to_invoice = json.to_invoice;
+                this.l10n_es_unique_id = json.l10n_es_unique_id;
+            }
+            export_as_JSON() {
+                const res = super.export_as_JSON(...arguments);
+                res.to_invoice = this.is_to_invoice();
+                if (!res.to_invoice) {
+                    res.l10n_es_unique_id = this.l10n_es_unique_id;
+                }
+                return res;
+            }
+            export_for_printing() {
+                const result = super.export_for_printing(...arguments);
+                const company = this.pos.company;
+                result.l10n_es_unique_id = this.l10n_es_unique_id;
+                result.to_invoice = this.to_invoice;
+                result.company.street = company.street;
+                result.company.zip = company.zip;
+                result.company.city = company.city;
+                result.company.state_id = company.state_id;
+                const base_by_tax = this.get_base_by_tax();
+                for (const tax of result.tax_details) {
+                    tax.base = base_by_tax[tax.tax.id];
+                }
+                return result;
+            }
+        };
+    Registries.Model.extend(Order, L10NESOrder);
 });
