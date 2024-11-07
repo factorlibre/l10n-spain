334f06e60a3cb171b88bc31320e1121c9509c79a
Luis J. Salvatierra <luis.salvatierra@factorlibre.com>
2024-11-07 13:20:18 +0100

    [FIX] l10n_es_payment_order_confirming_abanca
    Error: website key not found in manifest in l10n_es_payment_order_confirming_abanca.

--- l10n_es_payment_order_confirming_abanca/__manifest__.py
+++ l10n_es_payment_order_confirming_abanca/__manifest__.py
@@ -2,24 +2,19 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
-    'name': 'Exportación de fichero bancario Confirming para Abanca',
-    'version': '11.0.1.0.0',
-    'author': 'Comunitea, '
-              'Odoo Community Association (OCA)',
-    'contributors': [
-        'Ramón castro ',
-        'Santi Argüeso <santi@comunitea.com>'
-        
+    "name": "Exportación de fichero bancario Confirming para Abanca",
+    "version": "11.0.1.0.0",
+    "author": "Comunitea, " "Odoo Community Association (OCA)",
+    "contributors": ["Ramón castro ", "Santi Argüeso <santi@comunitea.com>"],
+    "category": "Localisation/Accounting",
+    "website": "https://github.com/OCA/l10n-spain",
+    "license": "AGPL-3",
+    "depends": [
+        "l10n_es_base_confirming",
     ],
-    'category': 'Localisation/Accounting',
-    'license': 'AGPL-3',
-    'depends': [
-        'l10n_es_base_confirming',
+    "data": [
+        "data/account_payment_method.xml",
+        "views/account_payment_mode.xml",
     ],
-    'data': [
-        'data/account_payment_method.xml',
-        'views/account_payment_mode.xml',
-    ],
-    'installable': True,
+    "installable": True,
 }
-
